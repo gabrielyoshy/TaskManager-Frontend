8c39334db83cee63f36569c26d074e3b5bc17273
gabrielyoshy <gabriel.pedrozopetrazzini@gmail.com>
2020-03-04 17:49:25 +0100

    Revert "1"
    This reverts commit 915458832f1d11aca84c64b5059580c26cf5a271.

--- src/app/components/projekte/projekt-list/projekt-list.component.ts
+++ src/app/components/projekte/projekt-list/projekt-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, HostBinding, Inject } from "@angular/core";
 import { ServiceService } from "../../../Service/service.service";
 import { Router } from "@angular/router";
-<<<<<<< HEAD
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import {
   MatDialog,
@@ -13,9 +12,6 @@ export interface DialogData {
   animal: string;
   name: string;
 }
-=======
-import { Kunde } from "src/app/Models/Kunde";
->>>>>>> parent of 4e6309d... antes de empezar a armar el dashboard
 
 @Component({
   selector: "app-projekt-list",
@@ -23,7 +19,6 @@ import { Kunde } from "src/app/Models/Kunde";
   styleUrls: ["./projekt-list.component.css"]
 })
 export class ProjektListComponent implements OnInit {
-<<<<<<< HEAD
   //esto es para la prueba de las tareas
   isLinear = false;
   firstFormGroup: FormGroup;
@@ -33,8 +28,6 @@ export class ProjektListComponent implements OnInit {
   name: string;
 
   panelOpenState = false;
-=======
->>>>>>> parent of 4e6309d... antes de empezar a armar el dashboard
   @HostBinding("class") classes = "row";
 
   projekte: any = [];
