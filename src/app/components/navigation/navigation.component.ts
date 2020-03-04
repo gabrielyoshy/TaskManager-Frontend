4e6309d5ab76bc54adaaf9e2ca65a96bec1e3864
gabrielyoshy <gabriel.pedrozopetrazzini@gmail.com>
2020-03-04 11:57:23 +0100

    antes de empezar a armar el dashboard

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  selector: "app-navigation",
+  templateUrl: "./navigation.component.html",
+  styleUrls: ["./navigation.component.css"]
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
